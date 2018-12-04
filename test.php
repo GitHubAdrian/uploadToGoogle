d037498a295108a8fc5235c17d69d5eefa933235
Adrian <35819342+GitHubAdrian@users.noreply.github.com>
2018-12-04 19:16:58 +0100

    Important files

---
+++ test.php
@@ -0,0 +1,13 @@
+<body>        
+        <div class="w3-container w3-center w3-padding-24" style="background-color: FAFAFA;">
+            <div class="w3-container" style="margin: 10px 64px;">
+                <form action="fileTest.php" method="post" enctype="multipart/form-data">
+                    Select image to upload:
+                    <input type="file" name="fileToUpload" id="fileToUpload">
+                    <input type="submit" value="Upload Image" name="submit" id="modBtn">
+                </form>
+
+            </div>
+        </div>
+    </div>
+</body>
